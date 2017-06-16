317ba06ad4ae085f8a0d90fbfc8b78ab278dcf98
kevinpratama <kevinpratama_1998@yahoo.com>
2017-06-16 15:18:31 +0700

    Create new.php

---
+++ new.php
@@ -0,0 +1,18 @@
+{
+  "events": [
+      {
+        "replyToken": "QB3HOowR3K7yLrF5BRoH9lgKXbXJg4iQDq1rdw+qFhaI9Vnxu5P8qddpCwlTVjPGcsNxlpUTZyVnqS/m5zUXj8S2YxrxIs7gsO/XDixsl0ruQOlWJ6Yg1MRRpVJoSsGPKnRXHZhUhqv9KqO0DUDkVwdB04t89/1O/w1cDnyilFU=",
+        "type": "message",
+        "timestamp": 1462629479859,
+        "source": {
+             "type": "user",
+             "userId": "U68583b3e650b5d0648b6028899304f26"
+         },
+         "message": {
+             "id": "325708",
+             "type": "text",
+             "text": "Hello, world"
+          }
+      }
+  ]
+}
